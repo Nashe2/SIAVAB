2b0de1ce4a956cc8e0b69dd54cfaac9064dafb4a
Nashe <nasheog2@gmail.com>
2020-06-17 19:03:29 -0500

    arreglando seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,18 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
     	// Esto va a correr si usas el php artisan db:seed ó php artisan migrate:refresh --seed
-    	$this->call([
-    		// Si quieres borrar tu base de datos descomentas esto, esto servira si haces cambios drasticos en tus schemas y se ropen tus migraciones
-    		CleanTableSeeder::class,
-    		// De aquí en adelante tendras que poner los demas seeders para rellenar la tabla
-    		//Por el momento y por motivos practicos comentare lo de clean para no borrar todo y solo veas que se agregan los registros que pusimos
-    		PersonasSeeder::class,
-    		//DistribuidorsSeeder:class 
-    	]);
 
-=======
         $this->call([
         	CleanTableSeeder::class,
 
@@ -45,6 +35,5 @@ class DatabaseSeeder extends Seeder
 
             ShoppingCartsSeeder::class,
         ]);
->>>>>>> 9bda8cdaac0303cd3f990e65aeb9ab7e8f9d224d
     }
 }
